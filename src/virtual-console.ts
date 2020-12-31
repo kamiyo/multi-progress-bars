77589749da18ef454fe435f090a49ba0c4dae752
Sean Chen <kamiyo@gmail.com>
2020-12-30 20:30:11 -0600

    fix(core): multi line logs were getting truncated because of faulty logic. Added some comments as well.

--- src/virtual-console.ts
+++ src/virtual-console.ts
@@ -17,15 +17,6 @@ export interface UpsertProgressOptions extends AddProgressOptions {
     refresh?: boolean;
 }
 
-// Fixed = managed by mpb
-type BufferType = 'fixed' | 'dynamic';
-
-interface BufferData {
-    type: BufferType;
-    data: string;
-    index: number;
-}
-
 export class VirtualConsole {
     private progressBuffer: string[];
     private consoleBuffer: string[];
@@ -55,6 +46,7 @@ export class VirtualConsole {
 
     cleanup() {
         this.stream?.write('\x1b[0m');
+        (console as any) = this.originalConsole;
     }
 
     init() {
@@ -70,15 +62,20 @@ export class VirtualConsole {
      * data: string
      */
     upsertProgress(options: UpsertProgressOptions) {
+
+        // If the progress we're upserting exists already, just update.
         if (options.index < this.progressHeight) {
             this.progressBuffer[options.index] = clampString(options.data, this.width);
             if (options.refresh === undefined || options.refresh) this.refresh();
             return;
         }
+
+        // Truncate progress line to console width.
         this.progressBuffer[options.index] = clampString(options.data, this.width);
+
+        // Extend the progress bars section, and reduce the corresponding console buffer height.
         const numToExtend = 1 + options.index - this.progressHeight;
         this.progressHeight = Math.max(options.index + 1, this.progressHeight);
-        // this.consoleHeight = this.height - this.progressHeight;
         if (numToExtend > 0) {
             this.consoleHeight -= numToExtend;
             const topLines = this.consoleBuffer.splice(0, numToExtend);
@@ -106,6 +103,7 @@ export class VirtualConsole {
         this.stream?.write(writeString);
     }
 
+    /* Prints out the buffers as they are */
     refresh() {
         const outString =
             CUP(0)
@@ -119,27 +117,31 @@ export class VirtualConsole {
 
     log(...data: any[]) {
         const writeString: string = format.apply(null, data);
+        // Split by newlines, and then split the resulting lines if they run longer than width.
         const clampedLines = writeString.split('\n').reduce<string[]>((prev, curr) => {
             const clamped = [];
             do {
                 const front = curr.slice(0, this.width);
                 curr = curr.slice(this.width);
                 clamped.push(front);
-            } while (curr.length > this.width)
+            } while (curr.length > 0)
             return [...prev, ...clamped];
         }, []);
+
         this.consoleBuffer.push(...clampedLines);
+
+        // If the console buffer is higher than console height, remove the top, and print them first.
         const topLines =
             (this.consoleBuffer.length > this.consoleHeight) ?
                 this.consoleBuffer.splice(0, this.consoleBuffer.length - this.consoleHeight) : [];
 
         const outString =
             CUP(0)
-            + topLines.map((val) => val + EL(EL_MODE.TO_END)).join('\n')
-            + (topLines.length ? '\n' : '')
-            + this.progressBuffer.map((val) => val + EL(EL_MODE.TO_END)).join('\n')
+            + topLines.map((val) => val + EL(EL_MODE.TO_END)).join('\n')    // print the previously removed top lines
+            + (topLines.length ? '\n' : '')                                 // separator
+            + this.progressBuffer.map((val) => val + EL(EL_MODE.TO_END)).join('\n')     // progress bars
             + (this.progressBuffer.length ? '\n' : '')
-            + this.consoleBuffer.map((val) => val + EL(EL_MODE.TO_END)).join('\n')
+            + this.consoleBuffer.map((val) => val + EL(EL_MODE.TO_END)).join('\n')      // rest of the console log.
             + '\n';
 
         this.stream?.write(outString);
